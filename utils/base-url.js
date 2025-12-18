632a2e8ae0fbfaf80c342c2383076fc55860b597
Hope <alwaysbeentaylor@users.noreply.github.com>
2025-12-18 17:42:04 +0100

    Fix Invalid URL error by trimming base URL

--- utils/base-url.js
+++ utils/base-url.js
@@ -7,16 +7,18 @@ export function getBaseUrl() {
 
     // 1. Manually configured base URL
     if (process.env.NEXT_PUBLIC_BASE_URL) {
-        url = process.env.NEXT_PUBLIC_BASE_URL.replace(/\/$/, '');
+        url = process.env.NEXT_PUBLIC_BASE_URL.trim().replace(/\/$/, '');
     }
     // 2. Vercel deployment URL
     else if (process.env.VERCEL_URL) {
-        url = `https://${process.env.VERCEL_URL}`;
+        url = `https://${process.env.VERCEL_URL.trim()}`;
     }
     // 3. Fallback for local development
     else {
         url = 'http://localhost:3000';
     }
+
+    url = url.trim();
 
     // Garandeer protocol
     if (url && !url.startsWith('http')) {
